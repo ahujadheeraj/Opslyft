700e0affee0c2943c34b3e4a495ab30e7eb99d3e
ahujadheeraj <ahujadheeraj99@gmail.com>
2019-09-03 21:04:09 +0530

    routing changed
    Signed-off-by: ahujadheeraj <ahujadheeraj99@gmail.com>

--- src/App.js
+++ src/App.js
@@ -1,20 +1,15 @@
 import React from 'react';
 import Home from './components/Home/Home'
-import Miniwaste from './components/Miniwaste/Miniwaste'
 
 import './App.css';
 import {BrowserRouter,Route} from 'react-router-dom'
 
 function App() {
   return (
-    <BrowserRouter>
       <div className="App">
-
-    <Route exact path = "/" component = {Home} />
-    <Route exact path = "/home" component = {Miniwaste} />
+        <Home />
 
       </div>
-    </BrowserRouter>
     
 
       
